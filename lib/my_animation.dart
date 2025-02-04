bf2ff7e40e07193baec9222526116cc46054b6e1
itajacy TI02 <itajacy@bsd.com.br>
2025-02-04 16:33:11 -0300

    AnimatedPositioned retirado e funcionando

--- lib/my_animation.dart
+++ lib/my_animation.dart
@@ -21,25 +21,21 @@ class _MyAnimationState extends State<MyAnimation> {
           AnimatedAlign(
             alignment: pressed ? Alignment.bottomRight : Alignment.topCenter,
             duration: Duration(milliseconds: 1000),
-            child: AnimatedPositioned(
-              curve: Curves.linear,
+            child: AnimatedContainer(
+              margin: EdgeInsets.all(16),
+              width: pressed ? 70 : 150,
+              height: 70,
               duration: Duration(milliseconds: 1000),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(pressed ? 35 : 0),
+                color: Colors.blue,
+              ),
               child: GestureDetector(
                 onTap: () {
                   setState(() {
                     pressed = !pressed;
                   });
                 },
-                child: AnimatedContainer(
-                  margin: EdgeInsets.all(16),
-                  width: pressed ? 70 : 150,
-                  height: 70,
-                  duration: Duration(milliseconds: 1000),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(pressed ? 35 : 0),
-                    color: Colors.blue,
-                  ),
-                ),
               ),
             ),
           ),
