32f7616b9d33132713832f354cfbf168a37e9170
itajacy TI02 <itajacy@bsd.com.br>
2025-02-04 13:52:57 -0300

    funcionando

--- lib/my_animation.dart
+++ lib/my_animation.dart
@@ -1,52 +1,77 @@
 import 'package:flutter/material.dart';
 
-class MyAnimation extends StatelessWidget {
+class MyAnimation extends StatefulWidget {
   const MyAnimation({super.key});
 
   @override
+  State<MyAnimation> createState() => _MyAnimationState();
+}
+
+class _MyAnimationState extends State<MyAnimation> {
+  bool pressed = true;
+
+  @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    print(size.height);
-    print(size.width);
-    bool change = true;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Exercício Animação 01'),
       ),
-      body: Container(
-        child: Stack(
-          children: [
-            Positioned(
-              right: size.width - (size.width - 50),
-              bottom: size.height - (size.height - 50),
-              // duration: Duration(seconds: 2),
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            curve: Curves.linear,
+            right: pressed ? 50 : 400,
+            bottom: pressed ? 50 : 800,
+            width: pressed ? 50 : 150,
+            height: 50,
+            duration: Duration(milliseconds: 1500),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  pressed = !pressed;
+                });
+              },
               child: Container(
-                width: 50,
+                width: pressed ? 50 : 150,
                 height: 50,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
+                  borderRadius: BorderRadius.circular(pressed ? 25 : 0),
                   color: Colors.blue,
                 ),
               ),
             ),
-            Positioned(
-              right: size.width - (size.width - 300),
-              bottom: size.height - (size.height - 800),
-              // duration: Duration(seconds: 2),
-              child: Align(
-                alignment: Alignment.center,
-                child: Container(
-                  height: 50,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
+          // Positioned(
+          //   right: size.width - (size.width - 50),
+          //   bottom: size.height - (size.height - 50),
+          //   // duration: Duration(seconds: 2),
+          //   child: Container(
+          //     width: 50,
+          //     height: 50,
+          //     decoration: BoxDecoration(
+          //       borderRadius: BorderRadius.circular(25),
+          //       color: Colors.blue,
+          //     ),
+          //   ),
+          // ),
+          // Positioned(
+          //   right: size.width - 200,
+          //   bottom: size.height - 200,
+          //   // duration: Duration(seconds: 2),
+          //   child: Align(
+          //     alignment: Alignment.center,
+          //     child: Container(
+          //       height: 50,
+          //       width: 150,
+          //       decoration: BoxDecoration(
+          //         color: Colors.red,
+          //       ),
+          //     ),
+          //   ),
+          // ),
